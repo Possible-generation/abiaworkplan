c585860e0e6d8a90af37f7e3ac7957da682a5332
Joshua Ajigbotosho <ajigbotoshojoshua2000@gmail.com>
2025-09-26 13:39:02 +0100

    hod fix

--- src/app/admin/hodd/dashboard/[dashboardId]/page.js
+++ src/app/admin/hodd/dashboard/[dashboardId]/page.js
@@ -1033,7 +1033,7 @@ export default function StaffDashboard({ params }) {
                       {/* {staffs.plans.length > 0
                         ? staffs.plans[0].created_at
                         : "-"} */}
-                      {staffs.plans?.length > 0
+                      {staffs?.plans?.length > 0
                         ? new Date(
                             staffs.plans[0].created_at
                           ).toLocaleDateString("en-US", {
